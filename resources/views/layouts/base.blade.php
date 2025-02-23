6f859d4582ef5fcb7d8c20d4fe6fe97281b27221
Ben Rowe <ben.rowe@covergenius.com>
2025-02-23 18:41:59 +1100

    Fix blade titles

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -2,9 +2,9 @@
 <html lang="en" class="h-full bg-gray-100">
 <head>
     <meta charset="utf-8">
-    <title>Release Bot</title>
+    <title>Same Day Racing</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Release Bot">
+    <meta name="description" content="Same Day Racing Bot">
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png">
     <link rel="stylesheet" href="/dist/output.css" media="">
