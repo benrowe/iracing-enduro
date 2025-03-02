24d02f914211611b606cd6cf9e5c824885a5a22d
Ben Rowe <ben.rowe@covergenius.com>
2025-03-03 10:02:06 +1100

    Update team spec

--- tests/E2E/teams.spec.ts
+++ tests/E2E/teams.spec.ts
@@ -2,13 +2,12 @@ import {expect, test} from "@playwright/test";
 
 
 test('has title', async ({ page }) => {
-    await page.goto('http://localhost/');
+    await page.goto(page.url());
 
-    // Expect a title "to contain" a substring.
-    console.log(await page.title())
     await expect(page).toHaveTitle(/Same Day Racing/);
 });
 
 test('can add team', async ({ page }) => {
-
+    await page.goto(page.url());
+    await expect(page).toHaveTitle(/Same Day Racing/);
 })
